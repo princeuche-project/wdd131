37fbf68553ff049d6ec52612031a4bbf36a2d9fa
uchenna33 <uowuamanamgodstime@byupathway.edu>
2026-05-06 01:58:32 +0400

    file

---
+++ scripts/getdate.js
@@ -0,0 +1,12 @@
+
+// Get current year
+const year = new Date().getFullYear();
+
+// Insert year into footer
+document.getElementById("year").textContent = `© ${year} 2026 Uchenna CopyRight Reserved`;
+
+// Get last modified date
+const lastModified = document.lastModified;
+
+// Insert last modified date
+document.getElementById("lastModified").textContent = `Last Modification: ${lastModified}`;
